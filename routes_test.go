536ffa87c634bbcc398b08ffa2efc2767648ec2a
Andre Gunhildberget <andgun96@gmail.com>
2018-10-09 10:57:03 +0200

    Added more tests

--- routes_test.go
+++ routes_test.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"testing"
@@ -9,7 +10,7 @@ import (
 
 var URL = "http://igcviewer-andregg.herokuapp.com" //Change to where the server are running
 
-func APIInfoRouteTest(t *testing.T) {
+func TestAPIInfoRoute(t *testing.T) {
 	//Check if the response contains correct fields and correct data
 	var testInfo apiInfo
 
@@ -20,10 +21,15 @@ func APIInfoRouteTest(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+
 	defer res.Body.Close()
 
 	decoder := json.NewDecoder(res.Body)
 	err = decoder.Decode(&testInfo)
+
+	if err != nil {
+		t.Error(err)
+	}
 
 	if testInfo.Version != "v1" || testInfo.Info != "Service for IGC tracks." {
 		t.Error("Unkown version or service")
@@ -31,18 +37,65 @@ func APIInfoRouteTest(t *testing.T) {
 
 }
 
-func IgcIDPostTest(t *testing.T) {
-	//Do a post request and check if we get a id in return
+func TestIgcIDPost(t *testing.T) {
+
+	var IGCURL Url
+	var IGCID Url_ID
+	IGCURL.Url = "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	var APIPostURL = URL + "/igcinfo/api/igc"
+
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(IGCURL)
+	res, err := http.Post(APIPostURL, "application/json; charset=utf-8", b)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&IGCID)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if IGCID.Id < -1 {
+		t.Error("ID is out of range.")
+	}
+
 }
 
-func IgcIDAllTest(t *testing.T) {
+func TestIgcIDAll(t *testing.T) {
 	//Check if we return a array with info
+
+	var APIPostURL = URL + "/igcinfo/api/igc"
+
+	var testArray []int
+
+	res, err := http.Get(APIPostURL)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&testArray)
+
+	if err != nil {
+		t.Error(err)
+	}
+
 }
 
-func IgcIDTest(t *testing.T) {
+func TestIgcID(t *testing.T) {
 	//See if we get correct info in return.
+
 }
 
-func IgcIDField(t *testing.T) {
+func TestIgcIDField(t *testing.T) {
 	//Check that info we get in return are correct
 }
