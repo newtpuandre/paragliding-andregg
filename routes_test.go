3d4f041cf3121bfd9538982aea6a7b914375fc34
Andre Gunhildberget <andgun96@gmail.com>
2018-10-09 09:49:13 +0200

    Actually add the test file...

---
+++ routes_test.go
@@ -0,0 +1,24 @@
+//Tests for routes.go file
+package main
+
+import "testing"
+
+func APIInfoRouteTest(t *testing.T) {
+	//Check if the response contains correct fields and correct data
+}
+
+func IgcIdPostTest(t *testing.T) {
+	//Do a post request and check if we get a id in return
+}
+
+func IgcIdAllTest(t *testing.T) {
+	//Check if we return a array with info
+}
+
+func IgcIdTest(t *testing.T) {
+	//See if we get correct info in return.
+}
+
+func IgcIdField(t *testing.T) {
+	//Check that info we get in return are correct
+}
