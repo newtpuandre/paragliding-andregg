bb9a36a824c4db4599be5b9ff4104bb501096f1f
André Gunhildberget <andregunhildberget@dhcp-10-22-198-86.wlan.ntnu.no>
2018-10-10 08:23:19 +0200

    Setup for testing

--- main.go
+++ main.go
@@ -17,6 +17,15 @@ func determineListenAddress() (string, error) { //Inorder to get the port heroku
 	return ":" + port, nil
 }
 
+func addRoutes(r *mux.Router) {
+	//Make the router handle routes. Routes located in routes.go
+	r.HandleFunc("/igcinfo/api", APIInfoRoute).Methods("GET")
+	r.HandleFunc("/igcinfo/api/igc", IgcIDPost).Methods("POST")
+	r.HandleFunc("/igcinfo/api/igc", IgcIDAll).Methods("GET")
+	r.HandleFunc("/igcinfo/api/igc/{igcId}", IgcID).Methods("GET")
+	r.HandleFunc("/igcinfo/api/igc/{igcId}/{igcField}", IgcField).Methods("GET")
+}
+
 func main() {
 
 	addr, err := determineListenAddress() //Get listening address
@@ -28,11 +37,7 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	//Make the router handle routes. Routes located in routes.go
-	router.HandleFunc("/igcinfo/api", APIInfoRoute).Methods("GET")
-	router.HandleFunc("/igcinfo/api/igc", IgcIDPost).Methods("POST")
-	router.HandleFunc("/igcinfo/api/igc", IgcIDAll).Methods("GET")
-	router.HandleFunc("/igcinfo/api/igc/{igcId}", IgcID).Methods("GET")
-	router.HandleFunc("/igcinfo/api/igc/{igcId}/{igcField}", IgcField).Methods("GET")
+	addRoutes(router)
 
 	//Log fatal errors and start the server
 	log.Fatal(http.ListenAndServe(addr, router))
