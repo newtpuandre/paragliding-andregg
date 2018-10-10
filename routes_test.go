cac02654fb8bef95c73abed5d84a9f8569021c84
André Gunhildberget <andregunhildberget@dhcp-10-22-198-86.wlan.ntnu.no>
2018-10-10 10:17:00 +0200

    test stuff

--- routes_test.go
+++ routes_test.go
@@ -16,32 +16,34 @@ func TestAPIInfoRoute(t *testing.T) {
 
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
 	// pass 'nil' as the third parameter.
-	req, err := http.NewRequest("GET", "/igcinfo/api", nil)
+	server := httptest.NewServer(http.HandlerFunc(APIInfoRoute))
+
+	req, err := http.Get(server.URL + "/igcinfo/api")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(APIInfoRoute)
+	var testInfo apiInfo
+	decoder := json.NewDecoder(req.Body)
+	decoderr := decoder.Decode(&testInfo)
 
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
+	if decoderr != nil {
+		t.Fatal(decoderr)
+	}
 
 	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
+	if status := req.StatusCode; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
 
 	// Check the response body is what we expect.
-	expected1 := `"info": "Service for IGC tracks."`
+	expected1 := "Service for IGC tracks."
 	expected2 := `"version": "v1"`
 
-	if strings.Contains(rr.Body.String(), expected1) && strings.Contains(rr.Body.String(), expected2) {
+	if testInfo.Info == expected1 && testInfo.Version == expected2 {
 		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected1+" "+expected2)
+			testInfo.Info+" "+testInfo.Version, expected1+" "+expected2)
 	}
 
 }
