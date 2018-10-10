ac7b0a34af1f689c1e055dc35f0758c67cae4bdc
André Gunhildberget <andregunhildberget@dhcp-10-22-198-86.wlan.ntnu.no>
2018-10-10 08:55:08 +0200

    Fixed another test

--- routes_test.go
+++ routes_test.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -43,33 +45,41 @@ func TestAPIInfoRoute(t *testing.T) {
 }
 
 func TestIgcIDPost(t *testing.T) {
-	/*
-		var IGCURL Url
-		var IGCID Url_ID
-		IGCURL.Url = "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
-		var APIPostURL = URL + "/igcinfo/api/igc"
 
-		b := new(bytes.Buffer)
-		json.NewEncoder(b).Encode(IGCURL)
-		res, err := http.Post(APIPostURL, "application/json; charset=utf-8", b)
+	var IGCURL Url
+	IGCURL.Url = "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	var APIPostURL = "/igcinfo/api/igc"
 
-		if err != nil {
-			t.Error(err)
-		}
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(IGCURL)
 
-		defer res.Body.Close()
+	req, err := http.NewRequest("POST", APIPostURL, b)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		decoder := json.NewDecoder(res.Body)
-		err = decoder.Decode(&IGCID)
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(IgcIDPost)
 
-		if err != nil {
-			t.Error(err)
-		}
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
 
-		if IGCID.Id < -1 {
-			t.Error("ID is out of range.")
-		}
-	*/
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected1 := `{"id": 0}`
+
+	if strings.Contains(rr.Body.String(), expected1) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected1)
+	}
+
 }
 
 func TestIgcIDAll(t *testing.T) {
