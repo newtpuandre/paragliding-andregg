02069f0a42bd24559c5687ad5e4cb5b47cd15334
André Gunhildberget <andregunhildberget@vpn-10-52-106-99.vpn-s.ntnu.no>
2018-10-25 14:47:15 +0200

    Error handling and other juicy changes

--- clockTrigger/main.go
+++ clockTrigger/main.go
@@ -3,6 +3,32 @@
 
 package clocktrigger
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
 
+func main() {
+	for t := range time.NewTicker(30 * time.Second).C { //SET TO 10 MIN
+		checkNewTracks(t)
+	}
+}
+
+func checkNewTracks(t time.Time) {
+	resp, err := http.Get("localhost:8080/paragliding/api/ticker/latest")
+	if err != nil {
+		// handle error
+	}
+	defer resp.Body.Close()
+
+	var timeStamp int64
+	decoder := json.NewDecoder(resp.Body)
+	decoderr := decoder.Decode(&timeStamp)
+	if decoderr != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(err)
 }
